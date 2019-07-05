bfe0a51752d117557fa46a7203b00717cdb376d8
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2019-07-05 15:18:56 +0200

    python-package: updated default calibration to hardware rev 1.3

--- software/python-package/shepherd/calibration_default.py
+++ software/python-package/shepherd/calibration_default.py
@@ -1,29 +1,41 @@
+# -*- coding: utf-8 -*-
+
 """
-This file contains some info about the hardware configuration on the shepherd
+shepherd.calibration_default
+~~~~~
+Contains some info about the hardware configuration on the shepherd
 cape. Based on these values, one can derive the expected adc readings given
 an input voltage/current or, for emulation, the expected voltage and current
 given the digital code in the DAC.
+
+
+:copyright: (c) 2019 by Kai Geissdoerfer.
+:license: MIT, see LICENSE for more details.
 """
 
 # both current channels have 511mOhm shunt resistance
-R_SHT = 0.511
-# the instrumentation amplifiers are configured for gain of 100
-G_INST_AMP = 100
+R_SHT = 2.0
+# the instrumentation amplifiers are configured for gain of 50.25
+G_INST_AMP = 50.25
 # we use the ADC's internal reference with 4.096V
 V_REF_ADC = 4.096
 # range of current channels is +/- 0.625 * V_REF
 G_ADC_I = 0.625
 # range of voltage channels is 1.25 * V_REF
 G_ADC_V = 1.25
-# DACs have external 3.0V reference
-V_REF_DAC = 3.0
+# DACs has internal 2.5V reference
+V_REF_DAC = 2.5
+# gain of DAC-A (current) is set to 1
+G_DAC_I = 1
+# gain of DAC-B (voltage) is set to 2
+G_DAC_V = 2
 # bias voltage is generated from V_REF_DAC based on 1.2k and 100k resistors
 G_BIAS_AMP = 1.2 / 100
 
 # resistor values used in current source
 R_CS_REF_L = 1200
-R_CS_REF_H = 510
-R_CS_OUT = 24
+R_CS_REF_H = 402
+R_CS_OUT = 30
 
 
 def current_to_adc(current: float):
@@ -41,12 +53,16 @@ def voltage_to_adc(voltage: float):
 
 
 def dac_to_voltage(value: int):
-    return value / (2 ** 18 - 1) * V_REF_DAC
+    return float(value) / (2 ** 16 - 1) * V_REF_DAC * G_DAC_V
+
+
+def voltage_to_dac(voltage: float):
+    return voltage / V_REF_DAC / G_DAC_V * (2 ** 16 - 1)
 
 
 def dac_to_current(value: int):
     # voltage at DAC output
-    v_dac = float(value) / (2 ** 18 - 1) * V_REF_DAC
+    v_dac = float(value) / (2 ** 16 - 1) * V_REF_DAC * G_DAC_I
     # voltage after biasing with differential amplifier
     v_dac_biased = v_dac - G_BIAS_AMP * V_REF_DAC
     # current through ground referenced path of current source
