a3c271af9a4b4134b64f86cafcd1d57151c4cd21
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2019-07-09 19:40:51 +0200

    Bump version: 0.0.1 → 0.0.2

--- software/shepherd-herd/setup.py
+++ software/shepherd-herd/setup.py
@@ -11,7 +11,7 @@ requirements = ["click", "fabric", "numpy", "pyYAML"]
 
 setup(
     name="shepherd_herd",
-    version="0.0.1",
+    version="0.0.2",
     description="Synchronized Energy Harvesting Emulator and Recorder CLI",
     long_description=README,
     long_description_content_type="text/markdown",
