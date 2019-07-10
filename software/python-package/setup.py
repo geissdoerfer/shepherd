462c4114e506f4e453dac52298e3844147148d2a
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2019-07-10 15:56:37 +0200

    Bump version: 0.0.2 → 0.0.3

--- software/python-package/setup.py
+++ software/python-package/setup.py
@@ -13,7 +13,7 @@ requirements = [
 
 setup(
     name="shepherd",
-    version="0.0.2",
+    version="0.0.3",
     description=("Synchronized Energy Harvesting" "Emulator and Recorder"),
     packages=["shepherd"],
     classifiers=[
