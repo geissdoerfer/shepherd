24fd0bf207a5be3f9c48f7a425042b9d2ffecc1c
Ingmar Splitt <orgua@users.noreply.github.com>
2020-08-29 00:04:14 +0200

    make define-magic safer to use

--- software/firmware/lib/include/intc.h
+++ software/firmware/lib/include/intc.h
@@ -8,11 +8,11 @@
  * See PRU-ICSS Reference Guide §5.2.2.2
  */
 #ifdef __GNUC__
-#define INTC_TRIGGER_EVENT(x)   write_r31((1U << 5U) + (x - 16U))
+#define INTC_TRIGGER_EVENT(x)   write_r31((1U << 5U) + ((x) - 16U))
 #else
-#define INTC_TRIGGER_EVENT(x)   __R31 = (1U << 5U) + (x - 16U)
+#define INTC_TRIGGER_EVENT(x)   __R31 = (1U << 5U) + ((x) - 16U)
 #endif
-#define INTC_CHECK_EVENT(x)     CT_INTC.SECR0 &(1U << x)
-#define INTC_CLEAR_EVENT(x)     CT_INTC.SICR_bit.STS_CLR_IDX = x;
+#define INTC_CHECK_EVENT(x)     (CT_INTC.SECR0 &(1U << (x)))
+#define INTC_CLEAR_EVENT(x)     (CT_INTC.SICR_bit.STS_CLR_IDX = (x));
 
 #endif /* __INTC_H_ */
