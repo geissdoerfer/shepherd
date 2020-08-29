48692970c8f476d7e94af0cc7594218d992a6e9e
Ingmar Splitt <orgua@users.noreply.github.com>
2020-08-29 02:38:15 +0200

    redo pru0-main: less global vars, more const, beter returns

--- software/firmware/pru0-shepherd-fw/main.c
+++ software/firmware/pru0-shepherd-fw/main.c
@@ -7,6 +7,7 @@
 #include <pru_intc.h>
 #include <rsc_types.h>
 
+#include "int_optimized.h"
 #include "gpio.h"
 #include "intc.h"
 #include "resource_table_def.h"
@@ -19,17 +20,11 @@
 #include "sampling.h"
 #include "shepherd_config.h"
 #include "virtcap.h"
+
 /* Used to signal an invalid buffer index */
 #define NO_BUFFER 0xFFFFFFFF
 
-static struct RingBuffer free_buffers;
-
-enum int_source_e { SIG_SAMPLE, SIG_BLOCK_END } int_source;
-
-volatile struct SharedMem *shared_mem =
-	(volatile struct SharedMem *)PRU_SHARED_MEM_STRUCT_OFFSET;
-
-static void send_message(uint32_t msg_id, uint32_t value)
+static void send_message(const uint32_t msg_id, const uint32_t value)
 {
 	struct DEPMsg msg_out;
 
@@ -38,15 +33,14 @@ static void send_message(uint32_t msg_id, uint32_t value)
 	rpmsg_putraw((void *)&msg_out, sizeof(struct DEPMsg));
 }
 
-uint32_t handle_block_end(volatile struct SharedMem *shared_mem,
-        struct RingBuffer *free_buffers_ptr,
-		struct SampleBuffer *buffers,
-		uint32_t current_buffer_idx,
-        uint32_t sample_idx)
+uint32_t handle_block_end(volatile struct SharedMem *const shared_mem,
+        struct RingBuffer *const free_buffers_ptr,
+		struct SampleBuffer *const buffers,
+		const uint32_t current_buffer_idx,
+        const uint32_t sample_idx)
 {
     uint32_t next_buffer_idx;
     uint8_t tmp_idx;
-	struct SampleBuffer *next_buffer;
 
 	/* Lock access to gpio_edges structure to avoid inconsistency */
 	simple_mutex_enter(&shared_mem->gpio_edges_mutex);
@@ -61,9 +55,9 @@ uint32_t handle_block_end(volatile struct SharedMem *shared_mem,
 	}
 
 	/* Fetch new buffer from ring */
-	if (ring_get(free_buffers_ptr, &tmp_idx) == 0) {
+	if (ring_get(free_buffers_ptr, &tmp_idx) > 0) {
 		next_buffer_idx = (unsigned int)tmp_idx;
-		next_buffer = buffers + next_buffer_idx;
+        struct SampleBuffer *const next_buffer = buffers + next_buffer_idx;
 		next_buffer->timestamp_ns = shared_mem->next_timestamp_ns;
 		next_buffer->gpio_edges.idx = 0;
 		shared_mem->gpio_edges = &next_buffer->gpio_edges;
@@ -77,8 +71,8 @@ uint32_t handle_block_end(volatile struct SharedMem *shared_mem,
 	return next_buffer_idx;
 }
 
-int32_t handle_rpmsg(struct RingBuffer *free_buffers_ptr, enum ShepherdMode mode,
-		 enum ShepherdState state)
+bool_ft handle_rpmsg(struct RingBuffer *const free_buffers_ptr, const enum ShepherdMode mode,
+		 const enum ShepherdState state)
 {
 	struct DEPMsg msg_in;
 
@@ -89,7 +83,7 @@ int32_t handle_rpmsg(struct RingBuffer *free_buffers_ptr, enum ShepherdMode mode
 	 * message
 	 */
 	if (rpmsg_get((void *)&msg_in) != sizeof(struct DEPMsg))
-		return 0;
+		return 1U;
 
 	_GPIO_TOGGLE(USR_LED1);
 
@@ -99,36 +93,36 @@ int32_t handle_rpmsg(struct RingBuffer *free_buffers_ptr, enum ShepherdMode mode
 		case MSG_DEP_DBG_ADC:
 			res = sample_dbg_adc(msg_in.value);
 			send_message(MSG_DEP_DBG_ADC, res);
-			return 0;
+			return 1U;
 
 		case MSG_DEP_DBG_DAC:
 			sample_dbg_dac(msg_in.value);
-			return 0;
+			return 1U;
 
 		default:
 			send_message(MSG_DEP_ERR_INVLDCMD, msg_in.msg_type);
-			return -1;
+			return 0U;
 		}
 	} else {
 		if (msg_in.msg_type == MSG_DEP_BUF_FROM_HOST) {
 			ring_put(free_buffers_ptr, (uint8_t)msg_in.value);
-			return 0;
+			return 1U;
 		} else {
 			send_message(MSG_DEP_ERR_INVLDCMD, msg_in.msg_type);
-			return -1;
+			return 0U;
 		}
 	}
 }
 
-void event_loop(volatile struct SharedMem *shared_mem,
-		struct RingBuffer *free_buffers_ptr,
-		struct SampleBuffer *buffers)
+void event_loop(volatile struct SharedMem *const shared_mem,
+		struct RingBuffer *const free_buffers_ptr,
+		struct SampleBuffer *const buffers)
 {
     uint32_t sample_idx = 0;
     uint32_t buffer_idx = NO_BUFFER;
+    static enum int_source_e { SIG_SAMPLE, SIG_BLOCK_END } int_source;
 
-	enum ShepherdMode shepherd_mode =
-		(enum ShepherdMode)shared_mem->shepherd_mode;
+	enum ShepherdMode shepherd_mode = (enum ShepherdMode)shared_mem->shepherd_mode;
 
 	while (1) {
 		/* Check if a sample was triggered by PRU1 */
@@ -146,8 +140,7 @@ void event_loop(volatile struct SharedMem *shared_mem,
 
 			/* The actual sampling takes place here */
 			if (buffer_idx != NO_BUFFER) {
-				sample(buffers + buffer_idx, sample_idx++,
-				       shepherd_mode);
+				sample(buffers + buffer_idx, sample_idx++, shepherd_mode);
 			}
 
 			if (int_source == SIG_BLOCK_END) {
@@ -160,10 +153,7 @@ void event_loop(volatile struct SharedMem *shared_mem,
 				if ((shared_mem->shepherd_state ==
 				     STATE_RUNNING) &&
 				    (shared_mem->shepherd_mode != MODE_DEBUG))
-					buffer_idx = handle_block_end(
-						shared_mem, free_buffers_ptr,
-						buffers, buffer_idx,
-						sample_idx);
+					buffer_idx = handle_block_end(shared_mem, free_buffers_ptr, buffers, buffer_idx, sample_idx);
 
 				sample_idx = 0;
 				_GPIO_OFF(USR_LED1);
@@ -182,21 +172,24 @@ void event_loop(volatile struct SharedMem *shared_mem,
 
 void main(void)
 {
-	/*
+    static struct RingBuffer free_buffers;
+
+    /*
 	 * The shared mem is dynamically allocated and we have to inform user space
 	 * about the address and size via sysfs, which exposes parts of the
 	 * shared_mem structure.
 	 * Do this initialization early! The kernel module relies on it.
 	 */
-	shared_mem->mem_base_addr = resourceTable.shared_mem.pa;
-	shared_mem->mem_size = resourceTable.shared_mem.len;
+    volatile struct SharedMem *const shared_mememory = (volatile struct SharedMem *)PRU_SHARED_MEM_STRUCT_OFFSET;
+	shared_mememory->mem_base_addr = resourceTable.shared_mem.pa;
+    shared_mememory->mem_size = resourceTable.shared_mem.len;
 
-	shared_mem->n_buffers = RING_SIZE;
-	shared_mem->samples_per_buffer = SAMPLES_PER_BUFFER;
-	shared_mem->buffer_period_ns = BUFFER_PERIOD_NS;
+    shared_mememory->n_buffers = RING_SIZE;
+    shared_mememory->samples_per_buffer = SAMPLES_PER_BUFFER;
+    shared_mememory->buffer_period_ns = BUFFER_PERIOD_NS;
 
-	shared_mem->harvesting_voltage = 0;
-	shared_mem->shepherd_mode = MODE_HARVESTING;
+    shared_mememory->harvesting_voltage = 0;
+    shared_mememory->shepherd_mode = MODE_HARVESTING;
 
 	/*
 	 * The dynamically allocated shared DDR RAM holds all the buffers that
@@ -204,13 +197,12 @@ void main(void)
 	 * This memory is requested from remoteproc via a carveout resource request
 	 * in our resourcetable
 	 */
-	struct SampleBuffer *buffers =
-		(struct SampleBuffer *)resourceTable.shared_mem.pa;
+	struct SampleBuffer *const buffers = (struct SampleBuffer *)resourceTable.shared_mem.pa;
 
 	/* Allow OCP master port access by the PRU so the PRU can read external memories */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
-	simple_mutex_exit(&shared_mem->gpio_edges_mutex);
+	simple_mutex_exit(&shared_mememory->gpio_edges_mutex);
 
 	/* Enable interrupts from PRU1 */
 	CT_INTC.EISR_bit.EN_SET_IDX = PRU_PRU_EVT_SAMPLE;
@@ -221,26 +213,24 @@ void main(void)
 	_GPIO_OFF(USR_LED1);
 	_GPIO_OFF(DEBUG_P0);
 
-	while (true) {
-		_GPIO_OFF(USR_LED1);
+reset:
+    _GPIO_OFF(USR_LED1);
 
-		if (shared_mem->shepherd_mode == MODE_VIRTCAP)
-			virtcap_init((struct VirtCapSettings *)&shared_mem
-					     ->virtcap_settings,
-				     (struct CalibrationSettings *)&shared_mem
-					     ->calibration_settings);
+    if (shared_mememory->shepherd_mode == MODE_VIRTCAP)
+        virtcap_init((struct VirtCapSettings *)&shared_mememory->virtcap_settings,
+                 (struct CalibrationSettings *)&shared_mememory->calibration_settings);
 
-		init_ring(&free_buffers);
-		sampling_init((enum ShepherdMode)shared_mem->shepherd_mode,
-			      shared_mem->harvesting_voltage);
-		shared_mem->gpio_edges = NULL;
+    init_ring(&free_buffers);
+    sampling_init((enum ShepherdMode)shared_mememory->shepherd_mode,
+              shared_mememory->harvesting_voltage);
+    shared_mememory->gpio_edges = NULL;
 
-		/* Clear all interrupt events */
-		CT_INTC.SICR_bit.STS_CLR_IDX = PRU_PRU_EVT_SAMPLE;
-		CT_INTC.SICR_bit.STS_CLR_IDX = PRU_PRU_EVT_BLOCK_END;
+    /* Clear all interrupt events */
+    CT_INTC.SICR_bit.STS_CLR_IDX = PRU_PRU_EVT_SAMPLE;
+    CT_INTC.SICR_bit.STS_CLR_IDX = PRU_PRU_EVT_BLOCK_END;
 
-		shared_mem->shepherd_state = STATE_IDLE;
+    shared_mememory->shepherd_state = STATE_IDLE;
 
-		event_loop(shared_mem, &free_buffers, buffers);
-	}
+    event_loop(shared_mememory, &free_buffers, buffers);
+    goto reset;
 }
