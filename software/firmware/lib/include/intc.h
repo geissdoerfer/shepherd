5680a129025a0fd4e0068449510c98a0e8865036
Ingmar Splitt <orgua@users.noreply.github.com>
2020-09-01 13:38:22 +0200

    allow for less jitter for sample()

--- software/firmware/lib/include/intc.h
+++ software/firmware/lib/include/intc.h
@@ -12,7 +12,11 @@
 #else
 #define INTC_TRIGGER_EVENT(x)   __R31 = (1U << 5U) + ((x) - 16U)
 #endif
+
 #define INTC_CHECK_EVENT(x)     (CT_INTC.SECR0 &(1U << (x)))
 #define INTC_CLEAR_EVENT(x)     (CT_INTC.SICR_bit.STS_CLR_IDX = (x));
+// below is a deconstructed INTC_CHECK_EVENT, better optimizable for slow/far register read
+#define INTC_GET_SECR0          (CT_INTC.SECR0)
+#define INTC_CHECK_EVENT_REG(reg, pos)  ((reg) &(1U << (x)))
 
 #endif /* __INTC_H_ */
