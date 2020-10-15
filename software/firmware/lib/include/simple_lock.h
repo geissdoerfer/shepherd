e81cec75f3e40c60fcd3d1722568af0c534aec93
Ingmar Splitt <orgua@users.noreply.github.com>
2020-10-15 11:58:04 +0200

    optimize simple-lock
    fixed width ints, const correctness, optimal data type

--- software/firmware/lib/include/simple_lock.h
+++ software/firmware/lib/include/simple_lock.h
@@ -2,11 +2,11 @@
 #define __SIMPLE_LOCK_H_
 
 #include <stdint.h>
-
+#include "stdint_fast.h"
 
 typedef struct {
-    uint32_t lock_pru0;
-    uint32_t lock_pru1;
+    bool_ft lock_pru0;
+    bool_ft lock_pru1;
 }__attribute__((packed)) simple_mutex_t;
 
 inline void simple_mutex_enter(volatile simple_mutex_t * mutex);
