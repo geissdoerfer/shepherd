e9af9d34e4072056a1430381469a8f37a1b75c26
Ingmar Splitt <orgua@users.noreply.github.com>
2020-10-15 11:51:16 +0200

    gcc support and faster io-ops

--- software/firmware/lib/include/gpio.h
+++ software/firmware/lib/include/gpio.h
@@ -1,49 +1,64 @@
 #ifndef __GPIO_H_
 #define __GPIO_H_
 
-volatile register uint32_t __R31;
+#ifdef __GNUC__ // NOTE: gcc can't use registers directly, so this is a workaround to allow gcc & cgt
+#include <pru/io.h>
+#else // CGT
 volatile register uint32_t __R30;
+volatile register uint32_t __R31;
+#define read_r30()      __R30
+#define write_r30(x)    __R30 = (x)
+#define read_r31()      __R31
+#define write_r31(x)    __R31 = (x)
+#endif
 
 #if defined(PRU0)
 
-    #define P8_11 15
-    #define P8_12 14
+    #define P8_11   15U
+    #define P8_12   14U
 
-    #define P9_25 7
-    #define P9_27 5
-    #define P9_28 3
-    #define P9_29 1
-    #define P9_30 2
-    #define P9_31 0
-    #define P9_41B 6
-    #define P9_42B 4
+    #define P9_25   7U
+    #define P9_27   5U
+    #define P9_28   3U
+    #define P9_29   1U
+    #define P9_30   2U
+    #define P9_31   0U
+    #define P9_41B  6U
+    #define P9_42B  4U
 
 #elif defined(PRU1)
 
-    #define P8_20 13
-    #define P8_21 12
+    #define P8_20   3U
+    #define P8_21   12U
 
-    #define P8_27 8
-    #define P8_28 10
-    #define P8_29 9
-    #define P8_30 11
+    #define P8_27   8U
+    #define P8_28   10U
+    #define P8_29   9U
+    #define P8_30   11U
 
-    #define P8_39 6
-    #define P8_40 7
-    #define P8_41 4
-    #define P8_42 5
-    #define P8_43 2
-    #define P8_44 3
-    #define P8_45 0
-    #define P8_46 1
+    #define P8_39   6U
+    #define P8_40   7U
+    #define P8_41   4U
+    #define P8_42   5U
+    #define P8_43   2U
+    #define P8_44   3U
+    #define P8_45   0U
+    #define P8_46   1U
 
 #else
     #error
 #endif
 
+#define BIT_SHIFT(x)    (1U << (x))
 
-#define _GPIO_TOGGLE(x) __R30 ^= (1 << x)
-#define _GPIO_ON(x) __R30 |= (1 << x)
-#define _GPIO_OFF(x) __R30 &= ~(1 << x)
+#ifdef __GNUC__
+#define GPIO_TOGGLE(x)  write_r30(read_r30() ^ (x))
+#define GPIO_ON(x)      write_r30(read_r30() | (x))
+#define GPIO_OFF(x)     write_r30(read_r30() & ~(x))
+#else // CGT
+#define GPIO_TOGGLE(x)  __R30 ^= (x)
+#define GPIO_ON(x)      __R30 |= (x)
+#define GPIO_OFF(x)     __R30 &= ~(x)
+#endif
 
 #endif /* __GPIO_H_ */
