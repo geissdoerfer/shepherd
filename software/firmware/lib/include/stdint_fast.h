903b62f753c743e411077b6b98af5132c0fed399
Ingmar Splitt <orgua@users.noreply.github.com>
2020-10-15 11:55:44 +0200

    new types -> fastest according to spec

---
+++ software/firmware/lib/include/stdint_fast.h
@@ -0,0 +1,29 @@
+#ifndef INT_OPTIMIZED_H
+#define INT_OPTIMIZED_H
+
+#include <stdint.h>
+
+// short-cuts for fastest data-types
+// - pru needs 7 cycles and branch when int8 or int16 or int32 is used together in an OP
+// - perfect for mixed usage and hinting minimal container size (needed per design)
+
+#define int8_ft     int32_t
+#define uint8_ft    uint32_t
+#define int16_ft    int32_t
+#define uint16_ft   uint32_t
+#define bool_ft     uint32_t
+
+/*
+const uint32_t cuint32_1 = 1;
+const uint16_t cuint16_1 = 1;
+const uint8_t  cuint8_1 = 1;
+const uint16_ft cuint16f_1 = 1;
+const uint8_ft  cuint8f_1 = 1;
+const int32_t cint32_1 = 1;
+const int16_t cint16_1 = 1;
+const int8_t  cint8_1 = 1;
+const int16_ft cint16f_1 = 1;
+const int8_ft  cint8f_1 = 1;
+*/
+
+#endif //INT_OPTIMIZED_H
