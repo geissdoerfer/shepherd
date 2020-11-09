3cd88815e78cd0c519defc0d4583f8d45093bdd2
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2020-11-09 18:56:22 +0100

    Bump version: 0.2.5 → 0.2.6

--- software/python-package/setup.py
+++ software/python-package/setup.py
@@ -21,7 +21,7 @@ if not os.getenv("READTHEDOCS"):
 
 setup(
     name="shepherd",
-    version="0.2.5",
+    version="0.2.6",
     description=("Synchronized Energy Harvesting" "Emulator and Recorder"),
     packages=["shepherd"],
     classifiers=[
