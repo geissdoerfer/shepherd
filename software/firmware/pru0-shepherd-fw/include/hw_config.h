866413a43783a7a849f6b06556ec9fe49c779137
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-01-08 00:29:26 +0100

    Update hw_config.h
    safer inlcudes

--- software/firmware/pru0-shepherd-fw/include/hw_config.h
+++ software/firmware/pru0-shepherd-fw/include/hw_config.h
@@ -10,9 +10,9 @@
 #if (SHEPHERD_VER == 1)
 
 // asm-routine needs unshifted bit positions (_PIN)
-#define SPI_CS_ADC_PIN     	P9_25
+#define SPI_CS_ADC_PIN     	(P9_25)
 #define SPI_CS_ADC_MASK		BIT_SHIFT(SPI_CS_ADC_PIN)
-#define SPI_CS_DAC_PIN      	P9_31
+#define SPI_CS_DAC_PIN      	(P9_31)
 #define SPI_CS_DAC_MASK 	BIT_SHIFT(SPI_CS_DAC_PIN)
 
 // make naming scheme compatible to v2
@@ -34,26 +34,29 @@
 #define DEBUG_PIN0_MASK 	BIT_SHIFT(P8_11)
 #define DEBUG_PIN1_MASK 	BIT_SHIFT(P8_12)
 
-#define VIRTCAP_OUT_PIN     	P9_29
+#define VIRTCAP_OUT_PIN     	(P9_29)
+#define VIRTCAP_OUT_MASK     	BIT_SHIFT(VIRTCAP_OUT_PIN)
 
 #elif (SHEPHERD_VER == 2)
 
-#define SPI_CS_REC_DAC_PIN      P9_27
+#define SPI_CS_REC_DAC_PIN      (P9_27)
 #define SPI_CS_REC_DAC_MASK	BIT_SHIFT(SPI_CS_REC_DAC_PIN)
-#define SPI_CS_REC_V_ADC_PIN    P9_41B
+#define SPI_CS_REC_V_ADC_PIN    (P9_41B)
 #define SPI_CS_REC_V_ADC_MASK 	BIT_SHIFT(SPI_CS_REC_V_ADC_PIN)
-#define SPI_CS_REC_C_ADC_PIN    P9_25
+#define SPI_CS_REC_C_ADC_PIN    (P9_25)
 #define SPI_CS_REC_C_ADC_MASK 	BIT_SHIFT(SPI_CS_REC_C_ADC_PIN)
 
-#define SPI_CS_EMU_DAC_PIN      P9_28
+#define SPI_CS_EMU_DAC_PIN      (P9_28)
 #define SPI_CS_EMU_DAC_MASK 	BIT_SHIFT(SPI_CS_EMU_DAC_PIN)
-#define SPI_CS_EMU_ADC_PIN      P9_42B
+#define SPI_CS_EMU_ADC_PIN      (P9_42B)
 #define SPI_CS_EMU_ADC_MASK 	BIT_SHIFT(SPI_CS_EMU_ADC_PIN)
 
+// Pins now share correct mapping with SPI1
 #define SPI_SCLK_MASK           BIT_SHIFT(P9_31)
 #define SPI_MOSI_MASK           BIT_SHIFT(P9_29)
 #define SPI_MISO_MASK           BIT_SHIFT(P9_30)
 
+// both pins have a LED
 #define DEBUG_PIN0_MASK         BIT_SHIFT(P8_12)
 #define DEBUG_PIN1_MASK         BIT_SHIFT(P8_11)
 
