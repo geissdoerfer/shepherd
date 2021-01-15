eeaeaba66b3076d1cbae5cffe35cff8c2d169c0e
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-01-15 01:16:37 +0100

    Update spi_transfer_pru.asm
    backport improved SPI-Fns

--- software/firmware/pru0-shepherd-fw/spi_transfer_pru.asm
+++ software/firmware/pru0-shepherd-fw/spi_transfer_pru.asm
@@ -1,3 +1,7 @@
+SCLK .set 3
+MOSI .set 2
+MISO .set 5
+
 .macro NOP
    MOV r23, r23
 .endm
@@ -11,31 +15,31 @@ adc_readwrite:
     LDI r14, 0 ; Clear return reg
 adc_outloop:
     SUB r20, r20, 1 ; decrement shiftloop counter
-    SET r30, r30, 3 ; Set SCLK high
+    SET r30, r30, SCLK ; Set SCLK high
     QBBC mosi_clear, r15, r20
-    SET r30, r30, 2 ; Set MOSI high
+    SET r30, r30, MOSI ; Set MOSI high
     JMP skip_mosi_clear
 mosi_clear:
-    CLR r30, r30, 2 ; Set MOSI low
+    CLR r30, r30, MOSI ; Set MOSI low
     NOP
 skip_mosi_clear:
     NOP
     NOP
-    CLR r30, r30, 3 ; Set SCLK low
+    CLR r30, r30, SCLK ; Set SCLK low
     NOP
     NOP
     NOP
     QBLT adc_outloop, r20, 0
-    CLR r30, r30, 2 ; clear MOSI
+    CLR r30, r30, MOSI ; clear MOSI
 adc_inloop:
-    SET r30, r30, 3 ; Set SCLK high
+    SET r30, r30, SCLK ; Set SCLK high
     SUB r21, r21, 1 ; decrement shiftloop counter
     NOP
     NOP
     NOP
     MOV r25, r31
-    CLR r30, r30, 3 ; Set SCLK low
-    QBBC adc_miso_clear, r25, 5
+    CLR r30, r30, SCLK ; Set SCLK low
+    QBBC adc_miso_clear, r25, MISO
     SET r14, r14, r21
     JMP skip_adc_miso_clear
 adc_miso_clear:
@@ -48,26 +52,30 @@ skip_adc_miso_clear:
     JMP r3.w2
 
 
-    .global dac_write
+    .global dac_write  ; code performs with 25 MHz, ~ 980 ns CS low
 dac_write:
     LDI r20, 24 ; Load Counter for outloop
+    SET r30, r30, SCLK ; Set SCLK high
     CLR r30, r30, r14 ; Set CS low
-    NOP
 
-dac_loop:
+dac_loop_head:
     SUB r20, r20, 1 ; Decrement counter
-    SET r30, r30, 3 ; Set SCLK high
-    QBBS set_mosi, r15, r20 ; If bit number [r20] is set in value [r15]
-    CLR r30, r30, 2 ; Set MOSI low
-    JMP skip_mosi_set
-set_mosi:
-    SET r30, r30, 2 ; Set MOSI high
+    QBBS dac_mosi_set, r15, r20 ; If bit number [r20] is set in value [r15]
+dac_mosi_clear:
+    SET r30, r30, SCLK ; Set SCLK high
+    CLR r30, r30, MOSI ; Set MOSI low
+    JMP dac_loop_tail
+dac_mosi_set:
+    SET r30, r30, SCLK ; Set SCLK high
+    SET r30, r30, MOSI ; Set MOSI high
     NOP
-skip_mosi_set:
-    CLR r30, r30, 3 ; Set SCLK low
+dac_loop_tail:
     NOP
-    QBLT dac_loop, r20, 0
-    CLR r30, r30, 2 ; clear MOSI
+    CLR r30, r30, SCLK ; Set SCLK low
+    QBLT dac_loop_head, r20, 0
+
+dac_end:
     NOP
+    CLR r30, r30, MOSI ; clear MOSI
     SET r30, r30, r14 ; set CS high
     JMP r3.w2
