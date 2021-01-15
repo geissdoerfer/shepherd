52f087a3c512c2753114a3910238d1b54530ae20
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-01-15 01:34:47 +0100

    Update spi_transfer_pru.asm
    improve adc-fn, more spi-conform (Mosi and CLK change together, duty cycle is equalized)

--- software/firmware/pru0-shepherd-fw/spi_transfer_pru.asm
+++ software/firmware/pru0-shepherd-fw/spi_transfer_pru.asm
@@ -11,43 +11,50 @@ adc_readwrite:
     MOV r24, r14 ; Save input arg (CS pin)
     LDI r20, 16 ; Load Counter for outloop
     LDI r21, 18; Load Counter for inloop
-    CLR r30, r30, r24 ; Set CS low
     LDI r14, 0 ; Clear return reg
-adc_outloop:
-    SUB r20, r20, 1 ; decrement shiftloop counter
     SET r30, r30, SCLK ; Set SCLK high
-    QBBC mosi_clear, r15, r20
+    CLR r30, r30, r24 ; Set CS low
+
+adc_outloop_head:
+    SUB r20, r20, 1 ; decrement shiftloop counter
+    QBBC adc_mosi_clear, r15, r20
+adc_mosi_set:
+    SET r30, r30, SCLK ; Set SCLK high
     SET r30, r30, MOSI ; Set MOSI high
-    JMP skip_mosi_clear
-mosi_clear:
+    JMP adc_outloop_tail
+adc_mosi_clear:
+    SET r30, r30, SCLK ; Set SCLK high
     CLR r30, r30, MOSI ; Set MOSI low
     NOP
-skip_mosi_clear:
+adc_outloop_tail:
+    NOP
     NOP
     NOP
     CLR r30, r30, SCLK ; Set SCLK low
     NOP
     NOP
+    QBLT adc_outloop_head, r20, 0
     NOP
-    QBLT adc_outloop, r20, 0
     CLR r30, r30, MOSI ; clear MOSI
-adc_inloop:
+adc_inloop_head:
     SET r30, r30, SCLK ; Set SCLK high
     SUB r21, r21, 1 ; decrement shiftloop counter
     NOP
     NOP
     NOP
-    MOV r25, r31
+    NOP
     CLR r30, r30, SCLK ; Set SCLK low
-    QBBC adc_miso_clear, r25, MISO
+    QBBC adc_miso_clear, r31, MISO
+adc_miso_set:
     SET r14, r14, r21
-    JMP skip_adc_miso_clear
+    JMP adc_inloop_tail
 adc_miso_clear:
     NOP
     NOP
-skip_adc_miso_clear:
+adc_inloop_tail:
     NOP
-    QBLT adc_inloop, r21, 0
+    QBLT adc_inloop_head, r21, 0
+adc_end:
     SET r30, r30, r24 ; set CS high
     JMP r3.w2
 
