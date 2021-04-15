250904769555f6f4a38aefa2757b416267111fd0
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2021-04-15 18:28:28 +0200

    Update pru_comm.c
    improve message system

--- software/kernel-module/src/pru_comm.c
+++ software/kernel-module/src/pru_comm.c
@@ -55,7 +55,7 @@ delayed_start_callback(struct hrtimer *timer_for_restart)
 
 	now_ns_system = (uint64_t)timespec_to_ns(&ts_now);
 
-	printk(KERN_INFO "shprd: Triggered delayed start@%llu", now_ns_system);
+	printk(KERN_INFO "shprd: Triggered delayed start @ %llu", now_ns_system);
 	return HRTIMER_NORESTART;
 }
 
@@ -119,38 +119,35 @@ unsigned int pru_comm_get_buffer_period_ns(void)
 
 unsigned char pru_comm_get_ctrl_request(struct CtrlReqMsg *const ctrl_request)
 {
+    static const uint32_t offset_msg = offsetof(struct SharedMem, ctrl_req);
+    static const uint32_t offset_unread = offsetof(struct SharedMem, ctrl_req) + offsetof(struct CtrlReqMsg, msg_unread);
+
     /* testing for unread-msg-token */
-    if (readb(pru_shared_mem_io + offsetof(struct SharedMem, ctrl_req) + 1) >= 1)
+    if (readb(pru_shared_mem_io + offset_unread) >= 1u)
     {
         /* if unread, then continue to copy request */
-        memcpy_fromio(ctrl_request,
-                pru_shared_mem_io + offsetof(struct SharedMem, ctrl_req),
-                sizeof(struct CtrlReqMsg));
+        memcpy_fromio(ctrl_request, pru_shared_mem_io + offset_msg, sizeof(struct CtrlReqMsg));
         /* mark as read */
-        writeb(0, pru_shared_mem_io + offsetof(struct SharedMem, ctrl_req) + 1);
+        writeb(0u, pru_shared_mem_io + offset_unread);
         return 1;
     }
     return 0;
 }
 
 
-
-unsigned char pru_com_set_ctrl_reply(struct CtrlRepMsg *const ctrl_reply)
+unsigned char pru_comm_send_ctrl_reply(struct CtrlRepMsg *const ctrl_reply)
 {
-    unsigned char status = readb(pru_shared_mem_io + offsetof(struct SharedMem, ctrl_rep) + 1) == 0;
+    static const uint32_t offset_msg = offsetof(struct SharedMem, ctrl_rep);
+    static const uint32_t offset_unread = offsetof(struct SharedMem, ctrl_rep) + offsetof(struct CtrlRepMsg, msg_unread);
+
+    unsigned char status = readb(pru_shared_mem_io + offset_unread) == 0u;
 
     /* first update payload in memory */
-    ctrl_reply->identifier = 0;
-    ctrl_reply->msg_unread = 0;
-    memcpy_toio(pru_shared_mem_io + offsetof(struct SharedMem, ctrl_rep),
-            ctrl_reply,
-            sizeof(struct CtrlRepMsg));
+    ctrl_reply->identifier = MSG_SYNC_CTRL_REP;;
+    ctrl_reply->msg_unread = 0u;
+    memcpy_toio(pru_shared_mem_io + offset_msg, ctrl_reply, sizeof(struct CtrlRepMsg));
 
-    /* activate message */
-    ctrl_reply->identifier = MSG_SYNC_CTRL_REP;
-    ctrl_reply->msg_unread = 1;
-    memcpy_toio(pru_shared_mem_io + offsetof(struct SharedMem, ctrl_rep),
-            ctrl_reply,
-            sizeof(struct CtrlRepMsg));
+    /* activate message with unread-token */
+    writeb(1u, pru_shared_mem_io + offset_unread);
     return status;
 }
